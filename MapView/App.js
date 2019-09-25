109982d6be4b415815f65768dd29b909d9c8361a
ashleynguci <trucanhftu2@gmail.com>
2019-09-25 13:14:07 +0300

    Fixed bug

--- MapView/App.js
+++ MapView/App.js
@@ -6,32 +6,31 @@ export default function App() {
   const [input, setInput] = useState("");
   const [region, setRegion] = useState({
     latitude: 60.200692,
-    longtitude: 24.934302,
+    longitude: 24.934302,
     latitudeDelta: 0.0322,
     longitudeDelta: 0.0221
   });
+
   const APP_KEY = "WNT6bPfGBpV5Xp4GBMGJ5dXsiX90rIyG";
-  const onShow = async () => {
-    try {
-      const response = await fetch(
-        `http://www.mapquestapi.com/geocoding/v1/batch?key=${APP_KEY}&location=${input}`
-      );
-      const data = await response.json();
-      setInput("");
-      await setRegion({
-        latitude: data.results[0].locations[0].displayLatLng.lat,
-        longitude: data.results[0].locations[0].displayLatLng.lng,
-        latitudeDelta: 0.0322,
-        longitudeDelta: 0.0221
+  const onShow = () => {
+    fetch(
+      `http://www.mapquestapi.com/geocoding/v1/batch?key=${APP_KEY}&location=${input}`
+    )
+      .then(response => response.json())
+      .then(data => {
+        setRegion({
+          latitude: data.results[0].locations[0].displayLatLng.lat,
+          longitude: data.results[0].locations[0].displayLatLng.lng,
+          latitudeDelta: 0.0322,
+          longitudeDelta: 0.0221
+        });
+      })
+
+      .catch(error => {
+        console.error(error);
       });
-    } catch (error) {
-      console.error(error);
-    }
   };
 
-  useEffect(() => {
-    onShow();
-  }, []);
   return (
     <View style={styles.container}>
       <MapView style={{ flex: 1 }} region={region}>
@@ -40,14 +39,14 @@ export default function App() {
             latitude: region.latitude,
             longitude: region.longitude
           }}
-          title="Haaga-Helia"
+          title="GOT YOU"
         />
       </MapView>
       <View>
         <TextInput
           style={{ width: 300, borderColor: "pink", borderWidth: 1 }}
-          onChangeText={text => setInput(text)}
           value={input}
+          onChangeText={text => setInput(text)}
         />
       </View>
       <View>
